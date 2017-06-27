090ff019e03fcf2aeae100ec2213b6883edea39e
umakusumuru <kusumuruuma@gmail.com>
2017-06-27 17:50:19 +0530

    payment type changed

--- WebapiApplication/DAL/PaymentDAL.cs
+++ WebapiApplication/DAL/PaymentDAL.cs
@@ -428,7 +428,7 @@ namespace WebapiApplication.DAL
 
         }
 
-        public ArrayList DgetProfilePaymentDetails_NewDesigns(long? intProfileID, string spName)
+        public ArrayList DgetProfilePaymentDetails_NewDesigns(string intProfileID, string spName)
         {
             int? intStatus = 0;
             DataSet dsPayment = new DataSet();
@@ -439,7 +439,7 @@ namespace WebapiApplication.DAL
             try
             {
                 parm[0] = new SqlParameter("@ProfileID", SqlDbType.BigInt);
-                parm[0].Value = intProfileID;
+                parm[0].Value = !string.IsNullOrEmpty(intProfileID)?Convert.ToInt64(intProfileID):0;
                 parm[1] = new SqlParameter("@Status", SqlDbType.Int);
                 parm[1].Direction = ParameterDirection.Output;
                 parm[2] = new SqlParameter("@ErrorMsg", SqlDbType.VarChar, 1000);
