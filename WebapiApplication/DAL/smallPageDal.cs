fb99d0c43bcf3fc6080eae7ad554122dc9c834b7
umakusumuru <kusumuruuma@gmail.com>
2017-08-12 11:17:23 +0530

    api of emp fact shhet re

--- WebapiApplication/DAL/smallPageDal.cs
+++ WebapiApplication/DAL/smallPageDal.cs
@@ -953,27 +953,27 @@ namespace WebapiApplication.DAL
             DataSet ds = new DataSet();
             try
             {
-                parm[0] = new SqlParameter("@dt_StartDate", SqlDbType.Int);
+                parm[0] = new SqlParameter("@dt_StartDate", SqlDbType.DateTime);
                 parm[0].Value = mobj.FromDate;
-                parm[1] = new SqlParameter("@dt_EndDate", SqlDbType.Structured);
+                parm[1] = new SqlParameter("@dt_EndDate", SqlDbType.DateTime);
                 parm[1].Value = mobj.toDate;
                 parm[2] = new SqlParameter("@t_ProfileOwner", SqlDbType.Structured);
                 parm[2].Value = Commonclass.returndt(mobj.Employeename, mobj.dtEmployeename, "empnames", "empIDsTable");
-                parm[3] = new SqlParameter("@t_Branch", SqlDbType.Int);
+                parm[3] = new SqlParameter("@t_Branch", SqlDbType.Structured);
                 parm[3].Value = Commonclass.returndt(mobj.Branch, mobj.dtBranch, "branch", "branchtable");
-                parm[4] = new SqlParameter("@i_PageSize", SqlDbType.DateTime);
+                parm[4] = new SqlParameter("@i_PageSize", SqlDbType.Int);
                 parm[4].Value = mobj.PageSize;
-                parm[5] = new SqlParameter("@i_PageNumber", SqlDbType.DateTime);
+                parm[5] = new SqlParameter("@i_PageNumber", SqlDbType.Int);
                 parm[5].Value = mobj.PageNumber;
                 parm[6] = new SqlParameter("@i_From", SqlDbType.Int);
                 parm[6].Value = mobj.SerialnoFrom;
                 parm[7] = new SqlParameter("@i_To", SqlDbType.Int);
                 parm[7].Value = mobj.SerialnoFrom;
-                parm[8] = new SqlParameter("@_Excel", SqlDbType.Int);
-                parm[8].Value = mobj.PageNumber;
-                parm[9] = new SqlParameter("@pagename", SqlDbType.Int);
+                parm[8] = new SqlParameter("@_Excel", SqlDbType.Bit);
+                parm[8].Value = mobj.flag;
+                parm[9] = new SqlParameter("@pagename", SqlDbType.VarChar);
                 parm[9].Value = mobj.Pagename;
-                parm[10] = new SqlParameter("@time", SqlDbType.Int);
+                parm[10] = new SqlParameter("@time", SqlDbType.VarChar);
                 parm[10].Value = mobj.timings;
 
                 ds = SQLHelper.ExecuteDataset(connection, CommandType.StoredProcedure, spname, parm);
