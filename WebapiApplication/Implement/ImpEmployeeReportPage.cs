31f8871df64a82049f6b31e7f64d65a806557d75
lakshmidevi <lakshmidevi407@gmail.com>
2017-12-01 12:56:58 +0530

    01_12_2017_bvankform

--- WebapiApplication/Implement/ImpEmployeeReportPage.cs
+++ WebapiApplication/Implement/ImpEmployeeReportPage.cs
@@ -246,7 +246,7 @@ namespace WebapiApplication.Implement
 
         public int? updateprofilebranchid(string Profileid) { return new EmployeeReportPageDAL().updateprofilebranchid(Profileid, "[dbo].[usp_updateprofilebranchid]"); }
 
-        public int? inserttorestoretable(long? Profileid) { return new EmployeeReportPageDAL().inserttorestoretable(Profileid, "[dbo].[usp_kumar_insertrecordintoallfields]"); }
+        public int? inserttorestoretable(long? Profileid) { return new EmployeeReportPageDAL().inserttorestoretable(Profileid, "[dbo].[usp_emp_InsertintoAllfields_AJS]"); }
     
     }
 
